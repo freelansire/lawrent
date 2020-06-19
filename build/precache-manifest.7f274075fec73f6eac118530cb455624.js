ae4416a0e44c08648870e3215f9ff638e23452aa
Samuel Moses <54817723+freelansire@users.noreply.github.com>
2020-06-19 12:55:19 -0700

    build folder

---
+++ build/precache-manifest.7f274075fec73f6eac118530cb455624.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b99dda01c1ea756ca909decc3357e350",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb1d110744213b1d6f89",
+    "url": "/static/css/main.c4f637de.chunk.css"
+  },
+  {
+    "revision": "5c52560148911d4591f8",
+    "url": "/static/js/2.4d67805f.chunk.js"
+  },
+  {
+    "revision": "eb1d110744213b1d6f89",
+    "url": "/static/js/main.135c117e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
